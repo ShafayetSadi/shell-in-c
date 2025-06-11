da34874adad6dbd89279d80cbe6cf08c80607790
Shafayet Sadi <shafayet.sadi@gmail.com>
2025-06-11 15:25:28 +0600

    The type builtin: builtins

--- src/main.c
+++ src/main.c
@@ -27,6 +27,22 @@ int main(int argc, char *argv[])
       return 0;
     else if (strncmp(input, "echo", 4) == 0)
       printf("%s\n", input + 5);
+    else if (strncmp(input, "type", 4) == 0)
+    {
+      char builtin_commands[][10] = {"echo", "exit", "type", "cd", "pwd", "ls"};
+      int is_builtin = 0;
+      for (int i = 0; i < sizeof(builtin_commands) / sizeof(builtin_commands[0]); i++)
+      {
+        if (strcmp(builtin_commands[i], input + 5) == 0)
+        {
+          printf("%s is a shell builtin\n", input + 5);
+          is_builtin = 1;
+          break;
+        }
+      }
+      if (!is_builtin)
+        printf("%s: not found\n", input + 5);
+    }
     else
       printf("%s: command not found\n", input);
   }
