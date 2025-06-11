bc160f1e84353f3de77242c6169eaa5804f89553
Shafayet Sadi <shafayet.sadi@gmail.com>
2025-06-11 14:49:39 +0600

    Handle invalid commands

--- src/main.c
+++ src/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
-
-  // Wait for user input
   char input[100];
+
+  printf("$ ");
   fgets(input, 100, stdin);
+  input[strcspn(input, "\n")] = 0;
+  printf("%s: command not found\n", input);
   return 0;
 }
